8f72d30359fb23f08250b08b125e3aa99ae1a83d
Maxime <maxime.caricand@co-assist.fr>
2021-04-14 21:55:21 +0200

    Mqtt message handling

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MQTTSensors from './model/mqttClient';
+
+new MQTTSensors().startMQTT('ws://random.pigne.org:9001');
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/model/mqttClient.js
@@ -0,0 +1,61 @@
+import { connect } from 'mqtt';
+import Sensor from './SensorModel';
+
+export default class MQTTSensors {
+
+    constructor() {
+        /** @type {Array<Sensor>} */
+        this._sensors = [];
+    }
+
+    get sensors() {
+        return this._sensors;
+    }
+
+    startMQTT(url) {
+        const client = connect(url);
+        client.on('connect', () => {
+            console.log('connected');
+            client.subscribe('value/+', (_, granted) => {
+                granted.forEach(entry => { // maybe useless ...
+                    console.log(`subscribed to ${entry.topic}`);
+                });
+            });
+        });
+        client.on('message', (_, message) => {
+            const data = JSON.parse(message);
+            this.updateSensorValues(data);
+        });
+    }
+
+    /**
+     * Return a sensor if it exist in the sensorList
+     * @param {string} name 
+     * @returns {Sensor}
+     */
+    getSensor(name) {
+        return this.sensors.find(sensor => sensor.name === name);
+    }
+
+    /**
+     * Return the list of all the sensors in the sensorList
+     * @returns {Array<string>}
+     */
+    getSensorsNames() {
+        return this._sensors.map(sensor => sensor.name);
+    }
+
+    /**
+     * If the sensor is in the sensorList, add a value, create the sensor otherwise
+     * @param {any} newSensor 
+     */
+    updateSensorValues(newSensor) {
+        const curentSensor = this.getSensor(newSensor.name);
+        if (curentSensor) {
+            curentSensor.addValue(newSensor.value);
+        } else {
+            this._sensors.push(new Sensor(newSensor));
+        }
+        console.log(this._sensors);
+    }
+}
