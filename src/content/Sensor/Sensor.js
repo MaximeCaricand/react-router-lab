b5197c99418640860a620869c9f6c9438091699b
Alexis L <alexis.labbe@etu.univ-lehavre.fr>
2021-04-23 16:54:19 +0200

    La valeur change selon le type de valeur du sensor

--- src/content/Sensor/Sensor.js
+++ src/content/Sensor/Sensor.js
@@ -1,24 +1,36 @@
 import styles from './Sensor.module.css'
 
 export default function Sensor(props) {
-    const currentValue = props.sensor.sensorValues.slice(-1)[0].value;
+    let currentValue = props.sensor.sensorValues.slice(-1)[0].value;
+    console.log(props.sensor)
+    switch(props.sensor.type){
+        case "TEMPERATURE": 
+            currentValue = Math.round(currentValue) + '  °F';
+            break;
+        case "PERCENT": 
+            currentValue = Math.round(currentValue*100) + ' %';
+            break;
+        case "OPEN_CLOSE": break;
+        default :
+    }
     return (
         <div>
             <div className={styles.name}>{props.sensor.name}</div>
             <div className={styles.text}>Valeur actuel :</div>
             <div className={styles.value}>{currentValue}</div>
             <div className={styles.text}>Historiques :</div>
-            <div className={styles.history}>{<TableHistory tab={props.sensor.sensorValues} />}</div>
+            <div className={styles.history}>{<TableHistory sensor={props.sensor} />}</div>
         </div>
     );
 }
 
 function TableHistory(props) {
     // slice limite l'affichage à 6 éléments
-    const rows = props.tab.sort((a, b) => b.date - a.date).slice(0, 6).map((sensorValue, index) => {
+    
+    const rows = props.sensor.sensorValues.sort((a, b) => b.date - a.date).slice(0, 6).map((sensorValue, index) => {
         return (
             <tr className={styles['ligne'+ (index % 2 ? "1" : "2")]} key={index}>
-                <td>{sensorValue.value}</td>
+                <td>{props.sensor.type === "PERCENT"?Math.round(sensorValue.value*100) :sensorValue.value}</td>
                 <td>{sensorValue.formatedDate}</td>
             </tr>
         )
